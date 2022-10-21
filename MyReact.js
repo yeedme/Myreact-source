ef00ab753f7bb50b740b547b6c60c7496bcc46b7
yeedme <73169342+yeedme@users.noreply.github.com>
2022-10-21 13:52:03 +0800

    fix:修复组件树型结构

--- MyReact.js
+++ MyReact.js
@@ -1,41 +1,54 @@
-import {todoObj,REACT_ELEMENT,REACT_TEXT} from "./untils.js";
+import { todoObj, REACT_ELEMENT, REACT_TEXT } from "./untils.js";
+
 function myCreateElement(type, config, children) {
-  // 
+  //
   let key = null;
   let ref = null;
-  let $$typeof = null;
-
- 
-
+  let $$typeof = REACT_ELEMENT;
   //判断config是否 key ref 。有就删除 便于后面 return
   if (config) {
-    key = config.key;
-    ref = config.ref;
+    key = config.key || null;
+    ref = config.ref || null;
     delete config.key;
     delete config.ref;
   }
-
   //获取config参数 浅拷贝对象 另一个用ES5 (For in )获取config对象属性
   let props = { ...config };
-  if (config) {
-    // arguments-2 就是减type confi
-    let childrenLength = arguments.length - 2;
-    // 分析children长度
-    if (childrenLength == 1) {
-      props.children = todoObj(children);   //todoObj 用于确认children类型 
-    } else if (childrenLength > 1) {
-      //aruments 是数组从 0开始
-      props.children = Array.prototype.slice.call(arguments, 2).map(todoObj); /// 可以解读为 map(funciton(elemtent)=>{ retrun .... } )
-    }
+
+  let childrenLength=arguments.length - 2;
+  if(childrenLength==1){ //只有节点
+    props.children=children;
+  }else if(childrenLength>1){
+    props.children = Array.prototype.slice.call(arguments, 2);
+  }else{
+    console.log("无节点");
   }
+  // 判断children 是否存在，如果存在 判断children类型
+
+  // if (children) {
+  //   if (typeof children === "string" || typeof children == "number") {
+  //     $$typeof = REACT_TEXT;
+     
+  //   } else {
+  //     $$typeof = REACT_ELEMENT;
+  //   }
+
+
+    // if (childrenLength == 1) {
+    //   props.children = children;
+    // } else if (childrenLength > 1) {
+    //   //aruments 是数组从 0开始
+    //    //
+    // }
+  // }
 
   return {
-    $$typeof: REACT_TEXT,
+    $$typeof,
     key, //唯一标,识
     ref, // Dom 实例
-    type, //H5类型  div span ....
+    type, //dom类型  div span ....
     props, // children 节点
   };
 }
-const react={myCreateElement}
-export {react};
+const react = { myCreateElement };
+export default react ;
