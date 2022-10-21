4ed8d847048242c5a4d9b7a4bb95d3f907bf08a5
yeedme <73169342+yeedme@users.noreply.github.com>
2022-10-21 13:59:51 +0800

    fix:修复render重复渲染组件

--- MyReact-dom.js
+++ MyReact-dom.js
@@ -1,58 +1,118 @@
-import { Children } from "react";
-import { REACT_ELEMENT, REACT_TEXT } from "./untils";
+
+
 
 function render(vdom, container) {
-  //vdom : 虚拟dom container:真实dom的存放位置
+  //vdom : 虚拟dom   container:真实dom的存放位置
+
+  // 创建真实dom ;
   const realDom = createDOm(vdom);
+
   container.appendChild(realDom);
 }
 
+  //将 vdom转化为 真实dom  ;
 function createDOm(vdom) {
-  //将 vdom转化为 真是dom  ;
-  const { type, props } = vdom;
-  console.log(vdom);
-  let dom;
-  // 将dom创建完毕
-  if (type == REACT_TEXT) {
-    dom = document.createTextNode(type);
-    console.log(dom);
-  } else {
-    dom = document.createElement(type);
-     dom.innerText=(props.children.content);
-     console.log(props.children.content);
+  let domNode;
+  let {props,type}=vdom;
+
+  if(typeof vdom == 'object'){
+    console.log("vdom是对象");
+    domNode=document.createElement(type);
+    updateProps(domNode,{},props);
+    let {children}=props;
+    if(children){
+        updateChildren(children,domNode)
+    }
   }
-  if (props) {
-    updateProps(dom, {}, props);
+  else if(typeof vdom == 'string' || 'number'){
+    domNode=document.createTextNode(vdom)
   }
-  return dom
+  // if (typeof vdom == "object") {
+  //   const { type, props } = vdom;
+  //   dom= document.createElement(type);
+  //   //判断 children类型   1 :纯文本  2:数组，数组首位可能是节点 或者是文本
+  //   if (typeof props.children === "string" || "number") {
+  //     let textDom = document.createTextNode(props.children);
+  //     console.log(props.children);
+  //     dom.appendChild(textDom);
+
+  //   } else if (
+  //     typeof props.children === "array" &&
+  //     (typeof props.children[0] === "string" || "number")
+  //   ) {
+  //     console.log(props.children[0]);
+  //     dom.innerText(props.children[0]);
+  //   }
+
+  //   if (props) {
+  //     updateProps(dom, {}, props);
+  //   }
+  //   let { children } = props;
+  //   // console.log("  let {children}=props",children);
+  //   if (children) {
+  //     updateChildren(children, dom);
+  //   }
+  // }else {
+  //   dom=document.createTextNode("")
+  // }
+  return domNode
+}
+
+function updateChildren(children,dom){
+    if(Array.isArray(children)){
+      children.forEach( item => {
+        render(item,dom)
+      })
+
+    }else {
+    render(children,dom)
+    }
 }
 
 //
 function updateProps(dom, oldProps, newProps) {
-  for (let key in newProps) {
-    if (key === 'children') {
-      continue;
-    } else if (key == "style") {
+  for(let key in newProps){
 
-      //获取style对象 再用for in  进行属性 赋值
-      let stylyObject = newProps[key];
-      for (let i in stylyObject) {
-        dom.style[i] = stylyObject[i]; // 赋予 dom style
+    if( key === 'children' ){     
+      continue;
+    }else if( key === 'style'){
+
+      let StyleObj=newProps[key];
+
+      for(let  i in StyleObj ){
+        dom.style[i]=StyleObj[i];
       }
 
-    } else {
-        console.log(key);
-        dom[key]=newProps[key]; // 将怕props上面的 属性赋予到 dom 上 。类似 className onclick之类的 
+    }else {
+      dom[key]=newProps[key];   
     }
   }
 
-  if(oldProps){
-        for(let key in oldProps){
-                if(!newProps[key]){
-                        dom[key]=null;
-                }
-        }
-  }
+
+  // for (let key in newProps) {
+  //   if (key === 'children') {
+  //     continue;
+  //   } else if (key == "style") {
+
+  //     //获取style对象 再用for in  进行属性 赋值
+  //     let stylyObject = newProps[key];
+  //     for (let i in stylyObject) {
+  //       dom.style[i] = stylyObject[i]; // 赋予 dom style
+  //     }
+
+  //   } else {
+  //       console.log(key);
+  //       dom[key]=newProps[key]; // 将怕props上面的 属性赋予到 dom 上 。类似 className onclick之类的 
+  //   }
+  // }
+
+  // if(oldProps){
+  //       for(let key in oldProps){
+  //               if(!newProps[key]){
+  //                       dom[key]=null;
+  //               }
+  //       }
+  // }
 }
 const reactDom = {
   render,
