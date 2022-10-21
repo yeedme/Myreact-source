1e169e3931342a8f9ee579a06c5418558536bd29
yeedme <73169342+yeedme@users.noreply.github.com>
2022-10-21 14:36:19 +0800

    fetch: 渲染函数组件

--- MyReact-dom.js
+++ MyReact-dom.js
@@ -1,118 +1,71 @@
-
-
-
 function render(vdom, container) {
   //vdom : 虚拟dom   container:真实dom的存放位置
 
   // 创建真实dom ;
-  const realDom = createDOm(vdom);
+  const realDom = createDom(vdom);
 
   container.appendChild(realDom);
 }
 
-  //将 vdom转化为 真实dom  ;
-function createDOm(vdom) {
+//将 vdom转化为 真实dom  ;
+function createDom(vdom) {
   let domNode;
-  let {props,type}=vdom;
+  let { props, type } = vdom;
 
-  if(typeof vdom == 'object'){
-    console.log("vdom是对象");
-    domNode=document.createElement(type);
-    updateProps(domNode,{},props);
-    let {children}=props;
-    if(children){
-        updateChildren(children,domNode)
+  //vdom是对象 说明夸你包含多个 ，也拥有自身的节点
+  if (typeof vdom == "object") {
+    domNode = document.createElement(type);
+    updateProps(domNode, {}, props);
+    let { children } = props;
+//渲染 子组件
+    if (children) {
+      updateChildren(children, domNode);
     }
   }
-  else if(typeof vdom == 'string' || 'number'){
-    domNode=document.createTextNode(vdom)
+  //判断是否函数组件
+  else if (typeof vdom === "function") {
+    domNode = renderFunction(vdom);
   }
-  // if (typeof vdom == "object") {
-  //   const { type, props } = vdom;
-  //   dom= document.createElement(type);
-  //   //判断 children类型   1 :纯文本  2:数组，数组首位可能是节点 或者是文本
-  //   if (typeof props.children === "string" || "number") {
-  //     let textDom = document.createTextNode(props.children);
-  //     console.log(props.children);
-  //     dom.appendChild(textDom);
-
-  //   } else if (
-  //     typeof props.children === "array" &&
-  //     (typeof props.children[0] === "string" || "number")
-  //   ) {
-  //     console.log(props.children[0]);
-  //     dom.innerText(props.children[0]);
-  //   }
-
-  //   if (props) {
-  //     updateProps(dom, {}, props);
-  //   }
-  //   let { children } = props;
-  //   // console.log("  let {children}=props",children);
-  //   if (children) {
-  //     updateChildren(children, dom);
-  //   }
-  // }else {
-  //   dom=document.createTextNode("")
-  // }
-  return domNode
+  //如果是文本就 添加进去当前节点
+  else if (typeof vdom == "string" || "number") {
+    domNode = document.createTextNode(vdom);
+  }
+  return domNode;
 }
 
-function updateChildren(children,dom){
-    if(Array.isArray(children)){
-      children.forEach( item => {
-        render(item,dom)
-      })
-
-    }else {
-    render(children,dom)
-    }
+//处理函数组件
+//  函数组件会 转化后 function XX(return react.createElement(....)) 这个时候只需要生成一个真实dom
+function renderFunction(funVdom) {
+  let vdom = funVdom();
+  return createDom(vdom);
 }
 
-//
+//处理children的虚拟DOM
+function updateChildren(children, dom) {
+  if (Array.isArray(children)) {
+    children.forEach((item) => {
+      render(item, dom);
+    });
+  } else {
+    render(children, dom);
+  }
+}
+
+//更新 props 。包括 style 鼠标 键盘事件
 function updateProps(dom, oldProps, newProps) {
-  for(let key in newProps){
-
-    if( key === 'children' ){     
+  for (let key in newProps) {
+    if (key === "children") {
       continue;
-    }else if( key === 'style'){
-
-      let StyleObj=newProps[key];
-
-      for(let  i in StyleObj ){
-        dom.style[i]=StyleObj[i];
+    } else if (key === "style") {
+      let StyleObj = newProps[key];
+// style样式  
+      for (let i in StyleObj) {
+        dom.style[i] = StyleObj[i];
       }
-
-    }else {
-      dom[key]=newProps[key];   
+    } else {
+      dom[key] = newProps[key];
     }
   }
-
-
-  // for (let key in newProps) {
-  //   if (key === 'children') {
-  //     continue;
-  //   } else if (key == "style") {
-
-  //     //获取style对象 再用for in  进行属性 赋值
-  //     let stylyObject = newProps[key];
-  //     for (let i in stylyObject) {
-  //       dom.style[i] = stylyObject[i]; // 赋予 dom style
-  //     }
-
-  //   } else {
-  //       console.log(key);
-  //       dom[key]=newProps[key]; // 将怕props上面的 属性赋予到 dom 上 。类似 className onclick之类的 
-  //   }
-  // }
-
-  // if(oldProps){
-  //       for(let key in oldProps){
-  //               if(!newProps[key]){
-  //                       dom[key]=null;
-  //               }
-  //       }
-  // }
 }
 const reactDom = {
   render,
